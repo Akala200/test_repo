774accc55e96f73ceec839e2718c7b771b292699
Akala Adeniyi <adeiyiakala91@gmail.com>
2025-03-12 11:23:47 +0100

    updated changes

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,6 @@
 import { SidebarNavItem, SiteConfig } from "types";
-import { env } from "@/env.mjs";
 
-const site_url = env.NEXT_PUBLIC_APP_URL;
+const site_url = process.env.NEXT_PUBLIC_APP_URL || "http://default-url.com";
 
 export const siteConfig: SiteConfig = {
   name: "Next Starter",
